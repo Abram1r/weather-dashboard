5bd34152a48a8b4bd08efe9f3de32b943d4688b8
Abram1r <103975640+Abram1r@users.noreply.github.com>
2022-06-14 13:11:28 -0500

    could not complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,40 @@
 var apiKey = "9e479cb68def2b3ab5f24efdac43555b";
-var city = document.querySelector("#cityInput");
-var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey
+var button = document.querySelector(".btn")
+var inputValue = document.querySelector(".inputValue")
+currentTemp = $("#temperature")
+currentHumidity = $("#humidity")
+currentWindSpeed = $("#windSpeed")
+currentUvi = $("#uvIndex")
+//var apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey
 
+// weather search request
 
-fetch(apiURL)
+button.addEventListener("click", function(){
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + inputValue + "&appid=9e479cb68def2b3ab5f24efdac43555b")
+     .then(response => response.json())
+     .then(data => { 
+       var nameValue = data["name"];
+       var tempValue = data["main"]["temp"];
+       var descValue = data["weather"][0]["description"];
 
+       name.innerHTML = nameValue;
+       temp.innerHTML = tempValue;
+       desc.innerHTML = descValue;
+     }) 
+
+    
+            
+    .catch(error => alert("wrong city name"))
+})
+    
+           // currentTemp.text(response.main),
+          //  currentHumidity.text(response.main.humidity + "%"),
+          //  currentWindSpeed.text(response.wind.speed + "MPH"),
+           // currentUvi.text(response.value))
+     
+
+//fetch(apiURL)
+//currentWeatherSearch()
 
 //"https://api.openweathermap.org/data/2.5/weather?q=San-Antonio&appid=9e479cb68def2b3ab5f24efdac43555b"
 //console.log(response.weather)
